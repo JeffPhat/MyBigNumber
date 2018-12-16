76797155e68dc67f27054860ecf31c6d56d3d732
JeffPhat <43369494+JeffPhat@users.noreply.github.com>
2018-12-16 10:45:49 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main{
+	
+	private Mybignumber number;
+main(){
+	number = new Mybignumber();
+	//nhập dữ liệu
+	String a;
+	String b;
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Input first number:");
+	a= sc.nextLine();
+	System.out.println("Input second number:");
+	b= sc.nextLine();
+	
+	String c = number.sum(a,b);
+	
+	System.out.println("Result is: "+c);
+}
