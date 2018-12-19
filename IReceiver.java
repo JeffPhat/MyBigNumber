020bc3fd3d0467770e4db57979e48cd7d19ece0a
JeffPhat <43369494+JeffPhat@users.noreply.github.com>
2018-12-19 21:06:10 +0700

    Add files via upload

---
+++ IReceiver.java
@@ -0,0 +1,10 @@
+package myjava.mybignumber;
+
+/**
+ * Tác giả: Trần Hữu Phát.
+ * sum là hàm để thực hiện phép cộng 2 chuỗi số
+ */
+public interface IReceiver {
+	
+	public abstract void send(String abc);
+}
